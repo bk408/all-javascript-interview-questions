a8e9b6c8248030093bffdfd9dc5aa5215fa717ee
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-08-15 20:58:41 +0530

    Write a for loop that prints every third number from 0 up to and including 99

---
+++ Write a for loop that prints every third number from 0 up to and including 99 using console.log/README.md
@@ -0,0 +1,3 @@
+
+
+# Write a for loop that prints every third number from 0 up to and including 99 using console.log
---
+++ Write a for loop that prints every third number from 0 up to and including 99 using console.log/index.js
@@ -0,0 +1,5 @@
+
+// Write a for loop that prints every third number from 0 up to and including 99 using console.log
+for (let i = 0; i <99; i+=3) {
+    console.log(i);
+}
